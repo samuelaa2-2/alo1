69c6496fb7fb3b75f664474a9b722d8cdea54fbf
samuelaa2-2 <samuelalejoandrade@gmail.com>
2025-01-12 22:14:19 -0500

    tallergrupal.c

--- tallergrupalS.A.c
+++ tallergrupalS.A.c
@@ -12,19 +12,19 @@ int main(int argc, char *argv[]) {
 
     printf("Ingrese una frase:");
     fflush(stdin);
-    fgets(frase, 50, stdin);
-    int len = strlen(frase);
+    fgets(frase, 50, stdin); //se usa fgets para que lean hasta 49 caracteres desde la entrada y el stdin los almacena 
+    int len = strlen(frase); //se usa strlen para calcular la longitud 
 
-    for (int i = 0; i < len; i++)
+    for (int i = 0; i < len; i++) //se inicia un bucle con la funcion for 
     {
 
-        printf("%c\n", frase[i]);
-        contadorn1++;
-        if (frase[i] == ' ' || frase[i] == '\n')
+        printf("%c\n", frase[i]); //se imprime un caracter por linea
+        contadorn1++; //se incrementa el contadorn1 
+        if (frase[i] == ' ' || frase[i] == '\n') //si es que se encuentran saltos de lineas se incrementa el contador
         {
             contador++;
-            numero = contadorn1 - 1;
-            contadorn1 = 0;
+            numero = contadorn1 - 1; 
+            contadorn1 = 0; //se reinicia a 0 para contar la siguiente palabra 
             if (numero < nmin)
             {
                 nmin = numero;
